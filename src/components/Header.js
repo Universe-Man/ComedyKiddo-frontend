d7b22c1ca2c643af8239018c5e466f868581bb17
github username <ianjpollack@gmail.com>
2018-07-25 11:08:27 -0400

    header only renders when logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,28 +29,38 @@ class Header extends React.Component {
 
   render(){
     return(
-      <div id='Header'>
-        <Menu className='menu-bar' >
-          <Menu.Item name='profile' active={this.state.activeItem === 'profile'} onClick={this.handleProfileClick} />
-          <Menu.Item
-            name='search'
-            active={this.state.activeItem === 'search'}
-            onClick={this.handleSearchClick}
-          />
-        <Menu.Menu position='right'>
-            <Menu.Item
-              name='logout'
-              active={this.state.activeItem === 'logout'}
-              onClick={this.handleLogoutClick}
-            />
-          </Menu.Menu>
-        </Menu>
-      </div>
+      <React.Fragment>
+        {(this.props.loggedIn === false) ? (null) : (
+          <div id='Header'>
+            <Menu className='menu-bar' >
+              <Menu.Item name='profile' active={this.state.activeItem === 'profile'} onClick={this.handleProfileClick} />
+              <Menu.Item
+                name='search'
+                active={this.state.activeItem === 'search'}
+                onClick={this.handleSearchClick}
+              />
+            <Menu.Menu position='right'>
+                <Menu.Item
+                  name='logout'
+                  active={this.state.activeItem === 'logout'}
+                  onClick={this.handleLogoutClick}
+                />
+              </Menu.Menu>
+            </Menu>
+          </div>
+        )}
+      </React.Fragment>
     )
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    loggedIn: state.loggedIn
+  }
+}
+
+export default connect(mapStateToProps)(Header);
 
 ///////////////////////
 
