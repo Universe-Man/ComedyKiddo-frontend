df02ebdb4ce52d450791ca4f2c07548e55de2c3d
github username <ianjpollack@gmail.com>
2018-07-25 11:00:46 -0400

    basic header up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,72 @@
 import React from 'react';
 import '../assets/App.css';
 import { connect } from 'react-redux';
+import { Input, Menu } from 'semantic-ui-react'
+
 
 class Header extends React.Component {
   constructor(){
     super();
-    this.state = {}
+    this.state = {
+      activeItem: 'profile'
+    }
+  }
+
+  handleProfileClick = () => {
+    console.log('profile!');
+    this.setState({ activeItem: 'profile' })
+  }
+
+  handleSearchClick = () => {
+    console.log('search!');
+    this.setState({ activeItem: 'search' })
+  }
+
+  handleLogoutClick = () => {
+    console.log('logout!');
+    this.setState({ activeItem: 'logout' })
   }
 
   render(){
     return(
       <div id='Header'>
-        Headers
+        <Menu className='menu-bar' >
+          <Menu.Item name='profile' active={this.state.activeItem === 'profile'} onClick={this.handleProfileClick} />
+          <Menu.Item
+            name='search'
+            active={this.state.activeItem === 'search'}
+            onClick={this.handleSearchClick}
+          />
+        <Menu.Menu position='right'>
+            <Menu.Item
+              name='logout'
+              active={this.state.activeItem === 'logout'}
+              onClick={this.handleLogoutClick}
+            />
+          </Menu.Menu>
+        </Menu>
       </div>
     )
   }
 }
 
 export default Header;
+
+///////////////////////
+
+///////////////////
+
+
+// export default class MenuExampleSecondary extends Component {
+//   state = { this.state.activeItem: 'home' }
+//
+//   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+//
+//   render() {
+//     const { activeItem } = this.state
+//
+//     return (
+//
+//     )
+//   }
+// }
